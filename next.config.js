8960faf639ec094308a8ec97b5a445b874a49801
Nikolinc <tsigulskynikita@gmail.com>
2023-12-21 19:44:16 +0500

    feat(next.config): add SVG loader and i18n configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const { i18n } = require('./next-i18next.config')
+
+const nextConfig = {
+  i18n,
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    )
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
+}
 
 module.exports = nextConfig
