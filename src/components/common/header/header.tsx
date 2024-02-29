b03c205d38f6459ffc2697d8777f32cc579e12bb
Nikolinc <tsigulskynikita@gmail.com>
2024-02-29 18:42:23 +0500

    feat(style): add buttonTypeOne style

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/ui/navbar/navbar";
-import LogoIcon from "@/shared/assets/icon/logo.png";
-import Image from "next/image";
+import LogoIcon from "@/shared/assets/icon/logo.svg";
 import Link from "next/link";
 import Style from "./header.module.css";
 
@@ -8,12 +7,12 @@ import Style from "./header.module.css";
 function Header() {
   return (
     <header className={Style.header}>
-      <Link href={"pages/home"}>
-        <Image src={LogoIcon} alt="Logo" width={30} />
+      <Link href={"home"}>
+        <LogoIcon className={Style.logoIcon} />
       </Link>
-      <Link href={"pages/home"} className="un">
-        <h2>
-          Cal'vani
+      <Link href={"home"} className="un">
+        <h2 className={Style.logoName}>
+          CAL'VANI
         </h2>
       </Link>
       <Navbar />
