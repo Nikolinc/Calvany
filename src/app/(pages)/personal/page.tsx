6438efa6e0017c37be1c12c54ff5c40a281d78c6
Nikolinc <tsigulskynikita@gmail.com>
2024-02-29 19:33:32 +0500

    reload:delete storyboock

--- src/app/(pages)/personal/page.tsx
+++ src/app/(pages)/personal/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Style from './page.module.css'
+import { sql } from "@vercel/postgres";
 
-function PersonalAccount() {
+export default async function PersonalAccount({
+  params
+} : {
+  params: { user: string }
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
   return (
-    <main>PersonalAccount</main>
-  )
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
 }
-
-export default PersonalAccount
