45c537f38309e85b8146122f7a01211a0ea7855f
Nikolinc <tsigulskynikita@gmail.com>
2024-03-04 20:40:32 +0500

    fix(common): fixed link

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -7,10 +7,10 @@ import Style from "./header.module.css";
 function Header() {
   return (
     <header className={Style.header}>
-      <Link href={"home"}>
+      <Link href={"/home"}>
         <LogoIcon className={Style.logoIcon} />
       </Link>
-      <Link href={"home"} className="un">
+      <Link href={"/home"} className="un">
         <h2 className={Style.logoName}>
           CAL'VANI
         </h2>
