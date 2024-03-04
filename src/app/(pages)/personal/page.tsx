e9d5c9b12023d5d69cab43e6f159dc34d9341df2
Nikolinc <tsigulskynikita@gmail.com>
2024-03-04 20:43:56 +0500

    reaload(page): clear code

--- src/app/(pages)/personal/page.tsx
+++ src/app/(pages)/personal/page.tsx
@@ -1,21 +1,7 @@
-import React from 'react'
-import Style from './page.module.css'
-import { sql } from "@vercel/postgres";
-
-export default async function PersonalAccount({
-  params
-} : {
-  params: { user: string }
-}): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
-
+async function PersonalAccount() {
   return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
-    </div>
-  );
+    <main>PersonalAccount</main>
+  )
 }
+
+export default PersonalAccount
