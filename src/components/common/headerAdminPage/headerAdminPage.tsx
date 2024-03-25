d6a0ba72b262550af1ff7bfbc6b21d3bc2ef921e
Nikolinc <tsigulskynikita@gmail.com>
2024-03-25 13:52:11 +0500

    reload(common):replaced the buttons with submit input and add translation to title

--- src/components/common/headerAdminPage/headerAdminPage.tsx
+++ src/components/common/headerAdminPage/headerAdminPage.tsx
@@ -1,35 +1,36 @@
-'use client'
-import Button from "@/components/ui/button/button"
-import IconBack from "@/shared/assets/icon/back.svg"
-import { useRouter } from 'next/navigation'
-import Style from './headerAdminPage.module.css'
+import Button from "@/components/ui/button/button";
+import IconBack from "@/shared/assets/icon/back.svg";
+import { useTranslations } from "next-intl";
+import Style from './headerAdminPage.module.css';
 
 interface headerAdminProps {
   TypePage: string,
   Tables: string,
-  path: string,
+  back: () => void,
 }
 
-function HeaderAdminPage(props: headerAdminProps) {
-  const router = useRouter();
+
+
+function TitleAdminPage(props: headerAdminProps) {
+  const t = useTranslations('Admin');
 
   return (
     <header className={Style.headerAdminPage}>
-      <Button className="buttonVoid" onClick={() => router.back()}><IconBack /></Button>
+      <Button className="buttonVoid" onClick={props.back}><IconBack /></Button>
       <h1>
-        {`${props.TypePage}  ${props.Tables[0].toUpperCase()}${props.Tables.substring(1)}`}
+        {` ${t(`Bar.${props.TypePage}`)} ${t(`Tables.${props.Tables}`)}`}
       </h1>
       <div className={Style.buttonSectionHAP}>
-        <Button className="buttonTypeHeader">
-          Сохранить
-        </Button>
-        <Button className="buttonTypeHeader" onClick={() => router.push(props.path)}>
-          Отмена
-        </Button>
+        <label className={Style.itemActionHeader}>
+          <input type="submit" placeholder={t(`General.save`)} />
+        </label>
+        <label className={Style.itemActionHeader}>
+          <input type="reset" placeholder={t(`General.cancel`)} />
+        </label>
       </div>
 
     </header >
   )
 }
 
-export default HeaderAdminPage
+export default TitleAdminPage
