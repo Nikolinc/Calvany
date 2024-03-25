5c031eb0a472e3d1c75ac900376f80e61ab701c7
Nikolinc <tsigulskynikita@gmail.com>
2024-03-25 13:53:36 +0500

    feat(api):add upload api

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,13 +1,5 @@
-import { put } from '@vercel/blob'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request): Promise<NextResponse> {
-  const { searchParams } = new URL(request.url)
-  const filename = searchParams.get('filename')
-
-  const blob = await put(filename as string, request.body!, {
-    access: 'public',
-  })
-
-  return NextResponse.json(blob)
+  return NextResponse.json(request)
 }
