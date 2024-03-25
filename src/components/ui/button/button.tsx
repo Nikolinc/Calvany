655a65edbdb73c1ac86afbf0a84aa4c603d65a55
Nikolinc <tsigulskynikita@gmail.com>
2024-03-25 13:48:06 +0500

    reload(ui):add translation to button

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,16 +1,22 @@
-import React from "react"
-import Style from './button.module.css'
+import { useTranslations } from "next-intl";
+import React from "react";
+import Style from './button.module.css';
 
 interface buttonPros {
-  children: React.ReactNode
+  children?: React.ReactNode
   className?: string
-  onClick?: (...params: any) => void
+  onClick?: (...params: any) => void,
+  pathLocalisation?: string;
+  text?: string
 }
 
 function Button(props: buttonPros) {
+  const t = useTranslations();
   return (
     <button className={`${Style[props.className!]} ${Style.button}`} onClick={props.onClick}>
-      {props.children}
+      {props.children ? (<>{props.children}</>)
+        : (<>{t(`${props.pathLocalisation}.${props.text}`)}</>)
+      }
     </button>
   )
 }
