4ba99784474e0e862c0e2fb2463e4d89f5060c26
Nikolinc <tsigulskynikita@gmail.com>
2024-03-25 13:42:12 +0500

    reload(ui):add translation to leftbar

--- src/components/ui/leftbar/leftbar.tsx
+++ src/components/ui/leftbar/leftbar.tsx
@@ -1,14 +1,16 @@
-import Link from "next/link"
-import Style from "./leftbar.module.css"
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import Style from "./leftbar.module.css";
 
 
 function Leftbar(props: { reference: string[] }) {
+  const t = useTranslations('Admin');
   return (
     <aside className={Style.LeftBar}>
-      <h2>Tables</h2>
+      <h2>{t('Bar.tables')}</h2>
       <nav className={Style.references}>
-        {props.reference.map((item) => (
-          <Link href={`/admin/${item}`} className="un" >{item}</Link>
+        {props.reference.filter((item) => item.indexOf("_") < 0).map((item) => (
+          <Link rel="prefetch" href={`/ru/admin/${item}/create`} as={`/ru/admin/${item}/create`} className="un">{t(`Tables.${item}`)}</Link>
         ))}
       </nav>
     </aside>
